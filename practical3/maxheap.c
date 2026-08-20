6e07d67a981b1e7f7b9cb2e5af9e2b357f146f22
Yash13ks <karenayash2006@gmail.com>
2026-08-20 12:59:17 +0530

    practical3 completed

--- practical3/maxheap.c
+++ practical3/maxheap.c
@@ -25,13 +25,20 @@ void heapify(int a[], int n, int i)
 
 int main()
 {
-    int a[] = {10, 20, 5, 30, 15};
-    int n = 5;
-    int i, temp;
+    int a[5], n, i, temp;
 
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+   
     for (i = n / 2 - 1; i >= 0; i--)
         heapify(a, n, i);
 
+    
     for (i = n - 1; i > 0; i--)
     {
         temp = a[0];
@@ -42,7 +49,6 @@ int main()
     }
 
     printf("Sorted array: ");
-
     for (i = 0; i < n; i++)
         printf("%d ", a[i]);
 
